f58ff2b98e2f31ae53b844e9added4753e7891cb
廖元垲 <Savent_gate@outlook.com>
2017-02-24 16:30:42 +0800

    first commit

--- PIC Data convert/PIC Data convert.cpp
+++ PIC Data convert/PIC Data convert.cpp
@@ -3,9 +3,22 @@
 
 #include "stdafx.h"
 
+static void printHelp(void);
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc < 2 || !strcmp(argv[1], "-h")) {
+		printHelp();
+	}
     return 0;
 }
 
+void printHelp(void)
+{
+	const char helpstring[] = "PIC Data Convert\n"
+		"\n"
+		"-T1 \t转换BMP格式文件到Type1:DTA格式（灰阶墨水屏文件格式）\n"
+		"-T2 \t转换两个BMP格式文件到Type2:ATD格式（双色墨水屏文件格式）\n"
+		"-h  \t帮助文件";
+	puts(helpstring);
+}
